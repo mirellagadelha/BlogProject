fdf8747c703e2e852a880c23d705504521b846a1
Mirella Gadelha <mirella.gadelha.s@gmail.com>
2018-10-19 01:25:03 -0300

    First Commit.

---
+++ database/migrations/2018_10_19_030915_create_table_news.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableNews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function(Blueprint $table)
+        {
+            $table->increments('id');
+
+            $table->string('title', 255);
+            $table->string('body', 255);
+            $table->string('category', 255);
+            $table->string('author', 255);
+            $table->string('keywords', 255);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('news');
+    }
+}
